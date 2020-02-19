a7dd1744d1e1910745bc38b97750eaecb62bb1a0
Vanya Shevchuk <aggressor2000@gmail.com>
2020-02-19 23:57:30 +0300

    adding opacity 0 to wrapper instead preloader, to fix loading bag, when all elements make a chaos!

--- js/preloader.js
+++ js/preloader.js
@@ -1,8 +1,11 @@
 document.body.onload = function () {
     const preloader = document.querySelector('.preloader');
+    const wrapper = document.querySelector('.wrapper');
+    wrapper.style.opacity = '0';
     setTimeout(function () {
         if(!preloader.classList.contains('done')){
             preloader.classList.add('done');
+            wrapper.style.opacity = '1';
         }
     }, 1500);
     setTimeout(function () {
