7a1376f5df952272c285128089ac917309ade2eb
Vanya Shevchuk <aggressor2000@gmail.com>
2020-03-07 17:44:26 +0300

    Add restart button, add body theme switcher and set options of body color to local storage.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,68 @@
 import nations from "./nations.js";
-
+const wrapper = document.querySelector('.content-container');
 const form = document.querySelector(".js-select-form");
 const playersSelect = document.querySelector(".js-people-amount");
 const nationsSelect = document.querySelector(".js-nations-amount");
 const startBtn = document.querySelector(".js-randomize");
 const listOfPlayers = document.querySelector(".js-list-of-players");
 const containerForNations = document.querySelector('.nations-container');
+const restart = document.createElement('button');
+const switchContainer = document.querySelector('.theme-switch');
+restart.textContent = 'Restart';
+restart.classList.add('random');
+
 startBtn.addEventListener("click", handleRandomizeClick);
+restart.addEventListener("click", handleRandomizeClick);
+
 let playersAmt;
 let nationsAmt;
 
+const switchBtn = document.querySelector(".js-switch-input");
+const Theme = {
+    LIGHT: "light-theme",
+    DARK: "dark-theme"
+};
+
+if (
+    localStorage.getItem("theme") &&
+    localStorage.getItem("theme") === "dark-theme"
+) {
+    document.body.classList.add(localStorage.getItem("theme"));
+    switchBtn.checked = true;
+}
+
+switchBtn.addEventListener("change", handleCheckBoxClick);
+function handleCheckBoxClick() {
+    if (switchBtn.checked) {
+        document.body.classList.remove(Theme.LIGHT);
+        document.body.classList.add(Theme.DARK);
+        localStorage.setItem("theme", Theme.DARK);
+    } else {
+        document.body.classList.remove(Theme.DARK);
+        document.body.classList.add(Theme.LIGHT);
+        localStorage.setItem("theme", Theme.LIGHT);
+    }
+}
+
 function handleRandomizeClick(e) {
     e.preventDefault();
+    listOfPlayers.innerHTML = '';
+    containerForNations.innerHTML = '';
     playersAmt = +playersSelect.value;
     form.classList.add("none");
-    document.body.style.color = "white";
     createPlayersMarkup(playersAmt);
+    wrapper.append(restart);
+    switchContainer.remove();
 }
 
 function createPlayersMarkup(playersAmount) {
     nationsAmt = +nationsSelect.value;
+    const arr = [...nations];
     for (let i = 1; i < playersAmount + 1; i++) {
         listOfPlayers.insertAdjacentHTML(
             "beforeend",
             `<li class="list-of-players_item"><p class="list-of-players_text">Player ${i}</p></li>`);
-        createNationsMarkup(nationsAmt, nations);
+        createNationsMarkup(nationsAmt, arr);
     }
 }
 
@@ -34,20 +72,24 @@ function createNationsMarkup(nationsAmount, arr) {
     let index;
     for (let i = 0; i < nationsAmount; i++) {
         if (!arrOfNation.join("").includes(arrOfNation[i])) {
-            listOfNations.classList.add('list-of-nations');
             const li = document.createElement("li");
-            li.classList.add('list-of-nations_item');
             const name = document.createElement("p");
             const img = document.createElement("img");
             let randomItem = arr[Math.floor(Math.random() * arr.length)];
+            listOfNations.classList.add('list-of-nations');
+            li.classList.add('list-of-nations_item');
             index = arr.indexOf(randomItem);
             arr.splice(index, 1);
             arrOfNation.push(randomItem.name);
-            name.textContent = randomItem.name;
-            img.src = randomItem.img;
-            li.append(img, name);
-            listOfNations.append(li);
+            appendItems(li, img, name, listOfNations, randomItem);
         }
     }
     containerForNations.append(listOfNations);
+}
+
+function appendItems(li, img, name, listOfNations, randomItem) {
+    name.textContent = randomItem.name;
+    img.src = randomItem.img;
+    li.append(img, name);
+    listOfNations.append(li);
 }
